9cc6bfd561b31985ec5438aa1c8706cbc2372272
Jason Edstrom <jason@instacode.io>
2021-06-18 18:58:35 -0500

    Added Course object to course detail view

--- Automation University/CourseDetail.swift
+++ Automation University/CourseDetail.swift
@@ -8,27 +8,34 @@
 import SwiftUI
 
 struct CourseDetail: View {
-    var body: some View {
+var course: Course
+        var body: some View {
         VStack{
-        HStack{
-        VStack(alignment: .leading) {
-            Text("Course Name").font(.title)
-            Text("Course Identifier").font(.subheadline)
-        }
+            Divider()
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Course Name").font(.title)
+                    Text("Course Identifier").font(.subheadline)
+                }
 
-            Spacer()
-            Text("Credits: 4").font(.largeTitle)
-        }
-            
-            
-            Text("Course Description").italic().bold()
-            
+                Spacer()
+                VStack {
+                    Text("4").font(.largeTitle)
+                    Text("CREDITS").font(.subheadline)
+                }
+            }
+            Divider()
+            VStack {
+                Text("Course Description").italic().bold()
+            }
         }
         .eraseToAnyView()
+    
+    
     }
 
     #if DEBUG
-    @ObservedObject var iO = injectionObserver
+        @ObservedObject var iO = injectionObserver
     #endif
 }
 
