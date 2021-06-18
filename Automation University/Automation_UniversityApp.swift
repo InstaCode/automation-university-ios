b8b593c513d97628b38117f986c5214844fe7b82
Jason Edstrom <jason@instacode.io>
2021-06-17 20:48:35 -0500

    Initial Commit

---
+++ Automation University/Automation_UniversityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Automation_UniversityApp.swift
+//  Automation University
+//
+//  Created by Jason Edstrom on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Automation_UniversityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
