5086b8b7741cc78bb08263bdd4c3dea488cd5582
Jason Edstrom <jason@instacode.io>
2021-06-19 15:36:18 -0500

    gitignore

---
+++ Automation University/Views/CourseRow.swift
@@ -0,0 +1,21 @@
+//
+//  CourseRow.swift
+//  Automation University
+//
+//  Created by Jason Edstrom on 6/19/21.
+//  Copyright © 2021 InstaCode. All rights reserved.
+//
+
+import SwiftUI
+
+struct CourseRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseRow()
+    }
+}
